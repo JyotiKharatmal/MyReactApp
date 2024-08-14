6900b9ea74fec3742928d2f0e306f3c4063b8850
JyotiKharatmal <jyoti.kharatmal@capgemini.com>
2024-08-14 17:33:27 +0530

    changes

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,15 +1,15 @@
-import React  from 'react';
+import React from 'react';
 
-class AboutPage extends React.Component{
-render(){
+class AboutPage extends React.Component {
+    render() {
         return (
-            <> <h1 >About</h1> 
-        <p>This is the about page.</p>
-        </>
-            
+            <> <h1 >About</h1>
+                <p>This is the about page.</p>
+            </>
+
         );
-        
-       }
+
+    }
 }
 
 export default AboutPage;
